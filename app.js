9b2a82ffabfe892a4c949ec40f2de77a2896322c
Vladimirov Denis <akakiy813@yandex.ru>
2019-11-05 21:25:10 +0300

    fix backend and add minimal front with handlebars

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
 const newsRoutes = require('./routes/news');
+const homeRoutes = require('./routes/home');
 
 const app = express();
 const limiter = rateLimit({
@@ -9,12 +13,23 @@ const limiter = rateLimit({
   max: 100
 });
 
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(helmet());
 app.use(limiter);
 
+app.use('/', homeRoutes)
 app.use('/news', newsRoutes);
 
 app.use('*', (req, res) => {
--- routes/news.js
+++ routes/news.js
@@ -4,6 +4,8 @@ const convert = require('xml-js');
 
 const router = Router();
 let resultTitle;
+let resultLink;
+let newsLink = [];
 
 fetch('https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en')  
     .then((res) => {
@@ -13,14 +15,21 @@ fetch('https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en')
         resultJson = convert.xml2json(data, {compact: true, spaces: 4});
         let objNews = JSON.parse(resultJson);
         resultTitle = objNews['rss']['channel']['item'].map(item => item.title._text)
+        resultLink = objNews['rss']['channel']['item'].map(item => item.link._text)
+        for (let i = 0; i < resultTitle.length; i++) { 
+            newsLink.push({ title: resultTitle[i], link: resultLink[i] })
+        }
     })
     .catch((error) => {  
         console.log('Request failed', error)  
-        });
+    });
 
 router.get('/', (req, res) => {
-    res.status(200).send(resultTitle);
+    res.render('news', {
+        title: 'Мировые новости',
+        isNews: true,
+        newsLink
+    })
 });
-
 
 module.exports = router;
